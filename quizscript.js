1a6660582a0931cc8bad4e8a8af87fdccbbddb58
calliefausey <31317882+calliefausey@users.noreply.github.com>
2017-09-06 16:00:32 -0700

    Update quizscript.js

--- quizscript.js
+++ quizscript.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	$("#p1").click(function() {
+		$("#p1").hide();
 		$("#a1").show();
 		$("#Aimg").slideDown("slow");
+		$("#p1").hide();
 	})
 	$("#p2").click(function() {
 		$("#a2").show();
